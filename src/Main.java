fb872043af37075fa2a91612e06d3b3f18ab9a03
IvanPopovych <popovytch.i@gmail.com>
2024-03-28 07:59:08 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+<<<<<<< HEAD
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
@@ -11,5 +12,7 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+=======
+>>>>>>> f76ef87 (First commit)
     }
 }
